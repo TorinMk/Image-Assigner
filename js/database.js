f3b144534a05831d08c02cbc1872c1db76e76af8
Torin Mallett <torin.mallett@netmatters-scs.com>
2026-02-13 10:33:22 +0000

    .

--- js/database.js
+++ js/database.js
@@ -16,13 +16,37 @@ $(document).ready(function () {
 
         console.log("Email has been saved: ", email);
         alert("Email saved successfully")
+        displaySavedImages(email);
+
     });
 });
+
+$('#save-image').on('click', function () {
+    const email = localStorage.getItem('userEmail');
+
+    if (!email) {
+        alert("Please submit an email first!")
+        return
+    }
+    if (!currentImageUrl) {
+        alert("Please generate an image first!");
+        return;
+    }
+
+    saveImage(email, currentImageUrl);
+    alert("Image saved");
+
+    displaySavedImages(email)
+})
 
 function saveImage(email, imageUrl) {
     
     // Gets images from localStorage
     const images = JSON.parse(localStorage.getItem('images')) || [];
+
+    // Stops duplicate images from generating
+    const exists = images.some(img => img.email === email && img.imageUrl === imageUrl);
+    if (exists) return;
 
     // Adds new image
     images.push({
@@ -35,4 +59,24 @@ function saveImage(email, imageUrl) {
     localStorage.setItem('images', JSON.stringify(images));
 
     console.log("Image saved for:", email);
+}
+
+function displaySavedImages(email) {
+    const container = $('#image-list');
+    container.html("")
+    
+    const images = JSON.parse(localStorage.getItem('images')) || [];
+
+    const filtered = images.filter(img => img.email === email);
+
+    if (filtered.length === 0) {
+        container.html(`<p>You have no saved images at the moment!</p>`);
+        return
+    }
+
+    filtered.forEach(img => {
+        container.append(`
+            <img src="${img.imageUrl}" width="200">
+            `);
+    });
 }
